97c8737aab8b0cc5ee210a8aa68de7c1a1a78cc3
johno4321 <josullivan@gmail.com>
2016-02-16 17:27:02 +0000

    checking in code

---
+++ DeleteAllFilesOlderThen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteAllFilesOlderThen
+{
+    public class DeleteAllFilesOlderThen
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(@"\\dub-backup01\SQLBACKUPS\DUB-DRSDBPRD01\FULL\DORIS");
+            var now = DateTime.Now;
+
+            foreach(var file in files)
+            {
+                var ts  = now.Subtract(File.GetLastAccessTime(file));
+                if(ts.Days > 2)
+                {
+                    File.Delete(file);
+                    //Console.WriteLine(string.Format("{0} {1}", file, File.GetLastAccessTime(file)));
+                }
+            }
+        }
+    }
+}
